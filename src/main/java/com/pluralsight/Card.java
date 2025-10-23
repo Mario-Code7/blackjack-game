275a55c1191ed41ff19e3875b9fb4d959360e359
Mario-Code7 <marioroa71@yahoo.com>
2025-10-23 10:05:29 -0500

    created classes for card, hand, deck, main

---
+++ src/main/java/com/pluralsight/Card.java
@@ -0,0 +1,69 @@
+package com.pluralsight;
+
+public class Card {
+    private String suit;
+    private String value;
+    private boolean isFaceUp;
+
+    public Card(String suit, String value, boolean isFaceUp) {
+        this.suit = suit;
+        this.value = value;
+        this.isFaceUp = false;
+    }
+
+    public String getSuit() {
+        if (isFaceUp) {
+            return suit;
+        } else {
+            return "#";
+        }
+    }
+
+
+    public String getValue() {
+        if (isFaceUp) {
+            return value;
+        } else {
+            return "#";
+        }
+    }
+
+    public int getPointValue() {
+        if (isFaceUp) {
+            switch (value) {
+                case "A":
+                    return 11;
+                case "J":
+                case "Q":
+                case "K":
+                    return 10;
+                default:
+                    try {
+                        return Integer.parseInt(value);
+                    } catch (NumberFormatException e) {
+                        return 0;
+                    }
+            }
+        } else {
+            return 0;
+        }
+    }
+
+
+    public boolean isFaceUp() {
+        return isFaceUp;
+    }
+
+    public void flip() {
+        isFaceUp = !isFaceUp;
+    }
+
+    @Override
+    public String toString() {
+        if (isFaceUp) {
+            return value + "of" + suit;
+        } else {
+            return "###";
+        }
+    }
+}
